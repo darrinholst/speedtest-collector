3ce8463b8c8cb17da18e40b8b62521adf0b48f9a
Darrin Holst <darrinholst@gmail.com>
2020-07-21 18:32:34 -0500

    initial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # speedtest-collector
+
+``` sh 
+while true; do curl -H "Content-Type: application/json" -d "$(speedtest --json)" http://localhost:3000/collect; sleep 870; done
+```
---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { google } = require("googleapis");
+const moment = require("moment");
+require("moment-timezone");
+
+const Mb = 1024 * 1024;
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/collect", async (req, res) => {
+  try {
+    const { body } = req;
+    const { timestamp, download, upload, ping } = body;
+    console.log(timestamp, download, upload);
+    const raw = JSON.stringify(body);
+    const email = process.env.GOOGLE_EMAIL;
+    const key = process.env.GOOGLE_KEY.replace(/\\n/g, '\n');
+    const spreadsheetId = process.env.GOOGLE_SPREADSHEET_ID;
+    const scopes = "https://www.googleapis.com/auth/spreadsheets";
+
+    const jwt = new google.auth.JWT(email, null, key, scopes);
+    await jwt.authorize();
+
+    await google
+      .sheets({ version: "v4", auth: jwt })
+      .spreadsheets.values.append({
+        spreadsheetId,
+        range: "Sheet1!A1:E1",
+        valueInputOption: "USER_ENTERED",
+        resource: {
+          values: [
+            [
+              moment(timestamp)
+                .tz("America/Chicago")
+                .format("YYYY-MM-DD HH:mm:ss"),
+              ping,
+              download / Mb,
+              upload / Mb,
+              raw,
+            ],
+          ],
+        },
+      });
+  } catch (e) {
+    console.error(e);
+  }
+
+  res.status(204).end();
+});
+
+app.listen(port, () => console.log(`listening at http://localhost:${port}`));
